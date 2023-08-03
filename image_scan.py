8e88c92ff8bd0b84ce229e18ddb38626039e3b1f
Eldar Tagiev <Tagiel2003@gmail.com>
2023-08-03 14:51:26 +0300

    tesseract to path

--- image_scan.py
+++ image_scan.py
@@ -27,10 +27,9 @@ def text_rec(image):
 def main(image):
     global text
 
-    # to use localy
-    # tesseract_path = r'C:\Program Files\Tesseract-OCR'
-    # # os.environ['PATH'] += os.pathsep + tesseract_path
-    # pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
+    tesseract_path = r'C:\Program Files\Tesseract-OCR'
+    os.environ['PATH'] += os.pathsep + tesseract_path
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
 
     preproc_img = image_preprocessing(image)
     img_text = text_rec(preproc_img)
