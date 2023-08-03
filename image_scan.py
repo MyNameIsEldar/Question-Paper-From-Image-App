dfc172b4cbf08064f4318f51f20bc9b2386609bd
Eldar Tagiev <Tagiel2003@gmail.com>
2023-08-03 14:48:51 +0300

    add tesseract to packages.txt

--- image_scan.py
+++ image_scan.py
@@ -27,9 +27,10 @@ def text_rec(image):
 def main(image):
     global text
 
-    tesseract_path = r'C:\Program Files\Tesseract-OCR'
-    # os.environ['PATH'] += os.pathsep + tesseract_path
-    pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
+    # to use localy
+    # tesseract_path = r'C:\Program Files\Tesseract-OCR'
+    # # os.environ['PATH'] += os.pathsep + tesseract_path
+    # pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
 
     preproc_img = image_preprocessing(image)
     img_text = text_rec(preproc_img)
