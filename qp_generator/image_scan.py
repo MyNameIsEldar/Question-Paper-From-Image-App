49183650f197604ce653ac38225aa3520eb376ad
Eldar Tagiev <Tagiel2003@gmail.com>
2023-08-04 21:43:42 +0300

    Update image_scan.py

--- qp_generator/image_scan.py
+++ qp_generator/image_scan.py
@@ -27,11 +27,11 @@ def text_rec(image):
 def main(image):
     global text
 
-    #pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract" 
+    pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract" 
     
-    tesseract_path = r'C:\Program Files\Tesseract-OCR'
-    os.environ['PATH'] += os.pathsep + tesseract_path
-    pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
+    # tesseract_path = r'C:\Program Files\Tesseract-OCR'
+    # os.environ['PATH'] += os.pathsep + tesseract_path
+    # pytesseract.pytesseract.tesseract_cmd = tesseract_path + r'\tesseract.exe'
 
     preproc_img = image_preprocessing(image)
     img_text = text_rec(preproc_img)
