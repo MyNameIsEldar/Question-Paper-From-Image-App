540171b8fbfb230a1717508c454f6a9d3b2d4ccd
Eldar Tagiev <Tagiel2003@gmail.com>
2023-10-14 17:05:43 +0300

    add vector base

--- qp_generator/test_generator.py
+++ qp_generator/test_generator.py
@@ -1,44 +1,44 @@
-
-# import config
 import app
 import streamlit as st
 import qp_generator.templates as templates
 from qp_generator.languages_data import languages
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
+    ChatPromptTemplate
 )
-from langchain.chains import LLMChain
-
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
 
 @st.cache_data
 def main(qp_type, text, tasks, answers, lang):
     OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
-    #openai_api_key=config.openai_api_key
-    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.7)
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0.5, model_name='gpt-3.5-turbo-16k')
 
     if qp_type == languages[lang]['test']:
         temp_part = templates.test
+        temp_part = temp_part.format(type=qp_type, num_of_q=tasks, num_of_a=answers)
     if qp_type == languages[lang]['open_questions']:
         temp_part = templates.open_questions
+        temp_part = temp_part.format(num_of_q=tasks)
 
-    template = """
-Ты полезный помошник-ассистент, который помогает пользователю составлять {type}. 
-    """ + temp_part
+    template = ("""
+Ты полезный помошник-ассистент, который помогает пользователю составлять {type}.""" 
++ temp_part) + "Текст: {text}"
+    
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
 
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-    human_template = "{text}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    texts = text_splitter.split_text(text)
 
-    chain = LLMChain(
-        llm=llm,
-        prompt=chat_prompt
-    )
+    db = Chroma.from_texts(texts, OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY))
+    retriever = db.as_retriever()
 
-    result = chain.run({'text': text, 'num_of_q': tasks, 'num_of_a': answers, 'type': qp_type})
+    prompt = ChatPromptTemplate.from_template(template)
 
+    rag_chain = {'text': retriever, 'type': RunnablePassthrough()} | prompt | llm 
+
+    result = rag_chain.invoke(qp_type).content
+    
     return result
